365badb05c12a2558ebaf51c8a68574061fd43cc
Dillon <dft609@gmail.com>
2021-01-22 12:10:33 -0700

    working backend

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const csurf = require('csurf');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
-
+const routes = require('./routes');
 const { environment } = require('./config');
 const isProduction = environment === 'production';
 
@@ -34,3 +34,7 @@ app.use(
     },
   })
 );
+
+app.use(routes)
+
+module.exports = app;
