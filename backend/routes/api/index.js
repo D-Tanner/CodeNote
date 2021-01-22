f169bb5da37bb18fc33f670e04bd130c99f0aaf9
Dillon <dft609@gmail.com>
2021-01-22 13:52:30 -0700

    more api routes

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,10 +1,17 @@
 const router = require('express').Router();
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
 const asyncHandler = require('express-async-handler');
 const { setTokenCookie } = require('../../utils/auth.js');
 const { User } = require('../../db/models');
 const { restoreUser } = require('../../utils/auth.js');
 const { requireAuth } = require('../../utils/auth.js');
 
+router.use('/session', sessionRouter);
+router.use('/users', usersRouter);
+
+
+//testing api routes
 router.get('/set-token-cookie', asyncHandler(async (req, res) => {
   const user = await User.findOne({
     where: {
