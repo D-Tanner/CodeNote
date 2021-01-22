c91ec55f90f614d63ee48c4d50744272c99c1267
Dillon <dft609@gmail.com>
2021-01-22 11:36:21 -0700

    adding backend

---
+++ backend/config/database.js
@@ -0,0 +1,52 @@
+const config = require('./index');
+
+const db = config.db;
+const username = db.username;
+const password = db.password;
+const database = db.database;
+const host = db.host;
+
+module.exports = {
+  development: {
+    username,
+    password,
+    database,
+    host,
+    dialect: 'postgres',
+    seederStorage: "sequelize",
+  },
+  production: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    seederStorage: 'sequelize',
+  },
+};
+
+
+
+// {
+//   "development": {
+//     "username": "root",
+//     "password": null,
+//     "database": "database_development",
+//     "host": "127.0.0.1",
+//     "dialect": "mysql",
+//     "operatorsAliases": false
+//   },
+//   "test": {
+//     "username": "root",
+//     "password": null,
+//     "database": "database_test",
+//     "host": "127.0.0.1",
+//     "dialect": "mysql",
+//     "operatorsAliases": false
+//   },
+//   "production": {
+//     "username": "root",
+//     "password": null,
+//     "database": "database_production",
+//     "host": "127.0.0.1",
+//     "dialect": "mysql",
+//     "operatorsAliases": false
+//   }
+// }
