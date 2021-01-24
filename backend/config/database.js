d384f1da2d4e97362cae2a2ff34ee8950cb9a0ea
Dillon <dft609@gmail.com>
2021-01-23 21:00:58 -0700

    testing SSL

--- backend/config/database.js
+++ backend/config/database.js
@@ -19,8 +19,20 @@ module.exports = {
     use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
     seederStorage: 'sequelize',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
 };
+  // production: {
+  //   use_env_variable: 'DATABASE_URL',
+  //   dialect: 'postgres',
+  //   seederStorage: 'sequelize',
+  // },
+// };
 
 
 
