d75a190a30364bd33021eb4eb8f540b080737c3b
Dillon <dft609@gmail.com>
2021-01-25 14:44:34 -0700

    awss3 file

--- backend/awsS3.js
+++ backend/awsS3.js
@@ -1,6 +1,6 @@
 const AWS = require("aws-sdk");
 // name of your bucket here
-const NAME_OF_BUCKET = "aws-s3-pern-demo";
+const NAME_OF_BUCKET = "codenoteapp";
 
 const multer = require("multer");
 
