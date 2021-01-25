5499b6185d89e11d4cc01d2ad5bc15ade84677de
Dillon <dft609@gmail.com>
2021-01-25 16:58:24 -0700

    thunk for global notes

---
+++ backend/routes/api/notes.js
@@ -0,0 +1,10 @@
+// const express = require('express');
+// const asyncHandler = require('express-async-handler');
+// const { Notes } = require('../../db/models')
+
+// const router = express.Router();
+
+
+// router.get('/api/notes/global', asyncHandler(async (req, res) => {
+
+// }))
