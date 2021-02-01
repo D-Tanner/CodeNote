f3c68f46f104710699810a2b395da41a4d70cc1c
Dillon <dft609@gmail.com>
2021-01-31 20:42:25 -0700

    styling landing page

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-// import ProfileButton from './ProfileButton';
+import image from './ImageWithText.PNG';
+import imageMatrix from './Matrix.png';
 import './Navigation.css';
 
 function Navigation({ isLoaded }) {
@@ -16,8 +17,20 @@ function Navigation({ isLoaded }) {
   } else {
     sessionLinks = (
       <>
-        <NavLink to="/login">Log In</NavLink>
-        <NavLink to="/signup">Sign Up</NavLink>
+        <div class="main-container">
+          <div class="login-signup-banner">
+            <img class="login-signup-logo" src={image} />
+          </div>
+          <div class="element-container">
+            <img class="matrix-image" src={imageMatrix} />
+
+          </div>
+          <div class="form-container">
+            <NavLink to="/login">Log In</NavLink>
+            <NavLink to="/signup">Sign Up</NavLink>
+
+          </div>
+        </div>
       </>
     );
   }
