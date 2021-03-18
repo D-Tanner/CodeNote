a5f18778c9866910b2592d1f671dce7a236415b9
Dillon <dft609@gmail.com>
2021-03-17 19:36:51 -0600

    css children

--- frontend/src/components/SearchBar/SearchBar.js
+++ frontend/src/components/SearchBar/SearchBar.js
@@ -17,7 +17,7 @@ const useStyles = makeStyles((theme) => ({
       backgroundColor: fade(theme.palette.common.white, 0.25),
     },
     marginLeft: 0,
-    width: '90%',
+    width: '80%',
     // [theme.breakpoints.up('sm')]: {
     //   marginLeft: theme.spacing(1),
     //   width: 'auto',
@@ -41,12 +41,12 @@ const useStyles = makeStyles((theme) => ({
     paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
     // transition: theme.transitions.create('width'),
     width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      width: '12ch',
-      '&:focus': {
-        width: '20ch',
-      },
-    },
+    // [theme.breakpoints.up('sm')]: {
+    //   width: '12ch',
+    //   '&:focus': {
+    //     width: '20ch',
+    //   },
+    // },
   },
 }));
 
@@ -107,11 +107,11 @@ const SearchBar = () => {
       {userId &&
         <div className={classes.search}>
           <div className={classes.searchIcon}>
-            <SearchIcon />
+            <SearchIcon className="search-icon" />
           </div>
           <InputBase
             placeholder="Search…"
-            className="no-border"
+            id="no-border"
             classes={{
               root: classes.inputRoot,
               input: classes.inputInput,
